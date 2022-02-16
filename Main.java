2ecf5e85ae5c7be01db557865691a0a03a268879
star_engine <jds4873@gmail.com>
2022-02-16 15:24:18 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,17 +8,31 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-        String pathToFile = new String(args[0]);
+        String pathToFile = new String("/uploads/subway_stops2.txt");
         File file = new File(pathToFile);
+        MyLinkedList list = new MyLinkedList();
        
         try (Scanner scanner = new Scanner(file)) {
              while (scanner.hasNext()) {
-                System.out.println(scanner.nextLine());
+                //System.out.println(scanner.nextLine());
+                list.addAtTail(scanner.nextLine());
              }
         } catch (FileNotFoundException e) {
                 System.out.println("No file found: " + pathToFile);
         }
        
-       
+        list.addAtHead("depot");
+        list.addAtTail("depot");
+        //list.printList();
+        //System.out.println("----------------------------------------");
+        //System.out.println("List size: " + list.size);
+        
+        displayLines(list);
+    }
+    
+    public static void displayLines(MyLinkedList list) {
+        for (int i = 0; i < list.size - 2; i++) {
+            System.out.println(list.get(i) + " - " + list.get(i + 1) + " - " + list.get(i + 2));
+        }
     }
 }
