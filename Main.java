0f441d89e890c9da33a311314dafdb7d83742fcb
star_engine <jds4873@gmail.com>
2022-03-24 17:27:21 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,35 +4,76 @@ import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.*;
 
-public class Main
-{
-    public static void main(String[] args) {
-        String pathToFile = new String("/uploads/subway_stops2.txt");
-        File file = new File(pathToFile);
-        MyLinkedList list = new MyLinkedList();
-       
-        try (Scanner scanner = new Scanner(file)) {
-             while (scanner.hasNext()) {
-                //System.out.println(scanner.nextLine());
-                list.addAtTail(scanner.nextLine());
-             }
-        } catch (FileNotFoundException e) {
-                System.out.println("No file found: " + pathToFile);
-        }
-       
-        list.addAtHead("depot");
-        list.addAtTail("depot");
-        //list.printList();
-        //System.out.println("----------------------------------------");
-        //System.out.println("List size: " + list.size);
-        
-        displayLines(list);
-    }
+public class Main {
     
-    public static void displayLines(MyLinkedList list) {
-        for (int i = 0; i < list.size - 2; i++) {
-            System.out.println(list.get(i) + " - " + list.get(i + 1) + " - " + list.get(i + 2));
+    public void getInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+    
+        String appendRegex = "(\\/append \"\\w+( \\w+)*\" \"*\\w+\"*|\\/append \"\\w+( \\w+)*\" \"\\w+( \\w+)\")";  //command "line" "station"
+        String addHeadRegex = "(\\/add-head \"\\w+( \\w+)*\" \"*\\w+\"*|\\/append \"\\w+( \\w+)*\" \"\\w+( \\w+)\")";
+        String removeRegex = "(\\/remove \"\\w+( \\w+)*\" \"*\\w+\"*|\\/append \"\\w+( \\w+)*\" \"\\w+( \\w+)\")";
+        String outputRegex = "\\/output \"\\w+( \\w+)*\"";
+        String exitRegex = "/exit";        
+        
+        while (true) {
+            if (userInput.isEmpty()) {
+                continue;
+            }
+            
+            if (userInput.matches(exitRegex)) {
+                return;
+            }
+            
+            if(userInput.matches(appendRegex)) {
+                ;
+            }
+            
+            if (userInput.matches(addHeadRegex)) {
+                ;
+            }
+            
+            if (userInput.matches(removeRegex)) {
+                ;
+            }
+            
+            if (userInput.matches(outputRegex)) {
+                
+            }
         }
+    }
+   
+    public static void main(String[] args) {
+        /*
+            -------------------------------------------------------------------        
+            Program Commands:
+            -------------------------------------------------------------------
+            /append "[line name]" "[station name]"  - adds a new station at the end of the line
+            /add-head "[line name]" "[station name]"    - adds a new station at the beginning
+            /remove "[line name]" "[station name]"  - removes a station
+            /output "[line name]"   - prints a line
+            /exit   - quit program
+           
+            -----------------------------------------------------------------------
+            A note about syntax :
+            -----------------------------------------------------------------------
+            If the name of the line or station consists of several words,
+            you should write it in quotation marks (after, they should be excluded from the name).
+            If the name of a station consists of one word it can be entered with or without quotes.
+        */
+       
+        String appendRegex = "(\\/append \"\\w+( \\w+)*\" \"*\\w+\"*|\\/append \"\\w+( \\w+)*\" \"\\w+( \\w+)\")";  //command "line" "station"
+        String addHeadRegex = "(\\/add-head \"\\w+( \\w+)*\" \"*\\w+\"*|\\/append \"\\w+( \\w+)*\" \"\\w+( \\w+)\")";
+        String removeRegex = "(\\/remove \"\\w+( \\w+)*\" \"*\\w+\"*|\\/append \"\\w+( \\w+)*\" \"\\w+( \\w+)\")";
+        String outputRegex = "\\/output \"\\w+( \\w+)*\"";
+        String exitRegex = "/exit";
+        Scanner scanner = new Scanner(System.in);
+        String testString = scanner.nextLine();
+        System.out.println("Input: " + testString);
+        //System.out.println("Regex: " + appendRegex1);
+        System.out.println("Matches: " + testString.matches(appendRegex1));
+        System.out.println("-------------------------------------------------------------------");
     }
 }
