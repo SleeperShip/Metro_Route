493afbda847d1463e1fb544ec9c8de50d20fb40f
star_engine <jds4873@gmail.com>
2022-03-24 11:32:31 -0600

    Update MyLinkedList.java
    Updated to Doubly-Linked list that holds Station objects, each with a Station name and the Line name it's a part of.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,122 +1,100 @@
 class MyLinkedList {
-    Node head;
-    Node tail;
+    
+    Node sentHead;
+    Node sentTail;
+    String listLine;
     int size;
-    
-    class Node {
-        String val;
-        Node next;
-        
-        public Node(String val) {
-            this.val = val;
-        }
-    }
-    
+   
     public MyLinkedList() {
-        head = null;
-        tail = null;
-        size = 0;
+        sentHead = new Node(0);
+        sentTail = new Node(0);
+        sentHead.next = sentTail;
+        sentHead.prev = null;
+        sentTail.prev = sentHead;
+        sentTail.next = null;
+        this.size = 0;
+    }
+   
+    class Node {
+        Node next;
+        Node prev;
+        String stationName;
+        String lineName;
+       
+        public Node(Node prevNode, Node nextNode, String station, String line) {
+            this.next = nextNode;
+            this.prev = prevNode;
+            this.stationName = station;
+            this.lineName = line;
+        }
+       
+        public Node(String station, String line) {
+            this.stationName = station;
+            this.lineName = line;
+        }
+    }
+   
+    public Node getNode(int index) {
+        Node currentNode = sentHead;
+       
+        for (int i = 0; i <= index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
     }
     
-    public String get(int index) {
+    public void addBetween(Node prevNode, Node nextNode, String station, String line) {
+        Node newNode = new Node(prevNode, nextNode, station, line);
+        prevNode.next = newNode;
+        nextNode.prev = newNode;
+        size++;
+    }
+
+    public int getStationName(int index) {
         if (index < 0 || index >= size) {
-            return "";
+            return -1;
         }
-        
-        Node nodeRef = head;
-        int n = 0;
-        
-        while (n < index) {
-            nodeRef = nodeRef.next;
-            n++;
-        }
-        return nodeRef.val;
+        return getNode(index).stationName;
     }
     
-    public void addAtHead(String val) {
-        Node newNode = new Node(val);
-        
-        newNode.next = head;
-        head = newNode;
-        if (tail == null) {
-            tail = head;
-        }
-        size++;
+    public void addAtHead(String station, String line) {
+        addBetween(sentHead, sentHead.next, station, line);
     }
-    
-    public void addAtTail(String val) {
-        Node newNode = new Node(val);
-        
-        if (head == null) {
-            addAtHead(val);
-            return;
-        }
-        
-        tail.next = newNode;
-        tail = newNode;
-        size++;
+   
+    public void addAtTail(String station, String line) {
+       addBetween(sentTail.prev, sentTail, val);
     }
-    
-    public void addAtIndex(int index, String val) {
-        if (index < 0 || index > size) {
-            return;
-        }
+   
+    public void addAtIndex(int index, String station, String line) {
+       if (index < 0 || index > size) {
+           return;
+       }
         
-        if (index == 0) {
-            addAtHead(val);
-            return;
-        }
+       if (index == size) {
+           addAtTail(String station, String line);
+           return;
+       } 
         
-        if (index == size) {
-            addAtTail(val);
-            return;
-        }
-        
-        Node nodeRef = head;
-        Node newNode = new Node(val);
-        
-        while (index > 1 && nodeRef != null) {
-            nodeRef = nodeRef.next;
-            index--;
-        }
-        
-        newNode.next = nodeRef.next;
-        nodeRef.next = newNode;
-        size++;
+       Node existingNode = getNode(index);
+       addBetween(existingNode.prev, existingNode, String station, String line); 
     }
-    
+   
     public void deleteAtIndex(int index) {
-        if (index < 0 || index >= size) {
-            return;
-        }
+       if (index < 0 || index >= size) {
+           return;
+       } 
         
-        if (index == 0) {
-            head = head.next;
-            size--;
-            return;
-        }
-        
-        Node nodeRef = head;
-        int n = 1;
-        
-        while (n < index && nodeRef != null) {
-                nodeRef = nodeRef.next;
-                n++;
-        }
-        
-        nodeRef.next = nodeRef.next.next;
-        if (nodeRef.next == null) {
-            tail = nodeRef;
-        }
-        size--;
+       Node nodeToDelete = getNode(index);
+       Node previousNode = nodeToDelete.prev;
+       Node nextNode = nodeToDelete.next;
+       previousNode.next = nextNode;
+       nextNode.prev = previousNode;
+       nodeToDelete.next = null;
+       nodeToDelete.prev = null;
+       size--;    
     }
     
-    public void printList() {
-        Node nodeRef = head;
-        
-        while (nodeRef != null) {
-            System.out.println(nodeRef.val);
-            nodeRef = nodeRef.next;
-        }
+    public String getListLine() {
+        return this.listLine;
     }
 }
